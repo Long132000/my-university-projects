059c395e664e46e8f3b6df651272fbbb1c6df259
unknown <user@A318WS6.ispo.spbstu.ru>
2025-09-04 13:56:45 +0300

    Реальное изменение в ветке hotfix

--- 6.cpp
+++ 6.cpp
@@ -1,9 +1,9 @@
-п»ї#include <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
-	string text = "РђР»Р»РµСЏ, СЃР°Рґ, Р°РїРµР»СЊСЃРёРЅ, Р°СЂР±СѓР·, Р·РѕРЅС‚";
+	string text = "Аллея, сад, апельсин, арбуз, зонт";
 
 	string word;
 	for (int i = 0;i<text.length();i++){
@@ -11,7 +11,7 @@ int main() {
 			word+=text[i];
 		}
 		else {
-			if (word.length() > 0 && (word[0] == 'Рђ' or word[0]== 'Р°')) {
+			if (word.length() > 0 && (word[0] == 'А' or word[0]== 'а')) {
 				cout<<word<<endl;
 				 
 			}
@@ -21,3 +21,5 @@ int main() {
 	}
 	return 0;
 }
+
+// Change from hotfix branch
